aa66284ab700abf4bfdec7e864cee91ae33ac567
amirreza <amirrezazand@hotmail.com>
2020-04-05 20:36:55 +0430

    fix User model

--- Users/models.py
+++ Users/models.py
@@ -5,5 +5,7 @@ from django.contrib.auth.models import User
 
 class user(User):
 
+    address = models.CharField(min_length=15, max_length=200, allow_blank=True)
+    postal_code = models.CharField(min_length=9, allow_blank=True)
     phone_number=models.CharField(max_length=15)
     img =models.ImageField(default='media/profile/images.png',upload_to='profile')
