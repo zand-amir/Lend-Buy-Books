1f733d3fbbafc730866adfa2e296f10f00b7f32a
amirreza <amirrezazand@hotmail.com>
2020-04-05 17:54:19 +0430

    Create User Model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class user(User):
+
+    phone_number=models.CharField(max_length=15)
+    img =models.ImageField(default='media/profile/images.png',upload_to='profile')
