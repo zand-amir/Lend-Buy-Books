7796725f55bda52ab8502a90c418f6d10fde639e
amirreza <amirrezazand@hotmail.com>
2020-04-05 17:56:02 +0430

    create signup serializer

---
+++ Users/api/serializers.py
@@ -0,0 +1,22 @@
+
+from django.contrib.contenttypes.models import ContentType
+
+from Users.models import user
+
+from rest_framework import serializers
+
+
+class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password=serializers.CharField()
+    email = serializers.EmailField(allow_blank=True)
+    first_name = serializers.CharField(allow_blank=True)
+    last_name = serializers.CharField(allow_blank=True)
+    phone_number=serializers.CharField(max_length=15,allow_blank=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user
+        fields = ('id','username', 'email', 'first_name', 'last_name', 'password','phone_number','img')
+
