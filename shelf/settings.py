ea0141519caf38f30f5a8f22a261afc186bce250
amirreza <amirrezazand@hotmail.com>
2020-04-05 18:02:11 +0430

    add user to settings.py

--- shelf/settings.py
+++ shelf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+
+    #local apps:
+    'Users'
 
 ]
 
