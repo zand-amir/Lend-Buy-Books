8fadded90c3dc336918ca0aa3bda683fc383592c
amirreza <amirrezazand@hotmail.com>
2020-04-06 18:27:38 +0430

    Add refresh and get token (JWT)

--- Users/urls.py
+++ Users/urls.py
@@ -4,6 +4,14 @@ from django.conf.urls import  url
 from rest_framework.authtoken import views
 from Users.views import SignupAPI
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     url(r'^sign-up/$', SignupAPI.as_view() , name='register'),
+    url(r'^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
 ]
