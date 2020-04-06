0c14c6f272709abd8c0f80cc36ba20fef1a462ab
amirreza <amirrezazand@hotmail.com>
2020-04-06 17:49:34 +0430

    fix user models.py bugs

--- Users/models.py
+++ Users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class user(User):
 
-    address = models.CharField(min_length=15, max_length=200, allow_blank=True)
-    postal_code = models.CharField(min_length=9, allow_blank=True)
+    address = models.CharField(max_length=200,   blank= True)
+    postal_code = models.CharField( max_length = 15 , blank=True)
     phone_number=models.CharField(max_length=15)
     img =models.ImageField(default='media/profile/images.png',upload_to='profile')
