413b0b049a4384824e9e6b8738b9b4b5e3a94008
amirreza <amirrezazand@hotmail.com>
2020-04-06 17:00:29 +0430

    fix signup serializer bug

--- Users/api/serializers.py
+++ Users/api/serializers.py
@@ -1,4 +1,3 @@
-
 from django.contrib.contenttypes.models import ContentType
 
 from Users.models import user
@@ -8,16 +7,18 @@ from rest_framework import serializers
 
 class SignupSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password=serializers.CharField()
+    password = serializers.CharField()
     email = serializers.EmailField(allow_blank=True)
     first_name = serializers.CharField(allow_blank=True)
     last_name = serializers.CharField(allow_blank=True)
-    phone_number = serializers.CharField(max_length=15 , allow_blank=True)
-    address = serializers.CharField(min_length = 15 , max_length=200, allow_blank=True)
+    phone_number = serializers.CharField(max_length=15, allow_blank=True)
+    address = serializers.CharField(min_length=15, max_length=200, allow_blank=True)
     postal_code = serializers.CharField(min_length=9, allow_blank=True)
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = ('id','username', 'email', 'first_name', 'last_name', 'password','phone_number', 'address' , 'postal_code', 'img')
-
+        fields = (
+            'id', 'username', 'email', 'first_name', 'last_name', 'password', 'phone_number', 'address', 'postal_code',
+            'img')
