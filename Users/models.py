596f3462212a9d3f2e6d412ffeac23f45ac140be
amirreza <amirrezazand@hotmail.com>
2020-04-06 17:56:41 +0430

    add rest_framework_simplejwt

--- Users/models.py
+++ Users/models.py
@@ -7,5 +7,5 @@ class user(User):
 
     address = models.CharField(max_length=200,   blank= True)
     postal_code = models.CharField( max_length = 15 , blank=True)
-    phone_number=models.CharField(max_length=15)
-    img =models.ImageField(default='media/profile/images.png',upload_to='profile')
+    phone_number = models.CharField(max_length=15)
+    img = models.ImageField(default='media/profile/images.png',upload_to='profile')
