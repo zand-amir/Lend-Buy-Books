02505def959f2d8ec25927d2864853193869343c
amirreza <amirrezazand@hotmail.com>
2020-04-07 17:05:00 +0430

    Add statics to User/url

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,14 @@
 
 
 from django.conf.urls import  url
+from django.urls import re_path
+from django.urls import path
 from rest_framework.authtoken import views
 from Users.views import SignupAPI
+
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,7 +17,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    url(r'^sign-up/$', SignupAPI.as_view() , name='register'),
-    url(r'^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
-]
+    url('sign-up/', SignupAPI.as_view(), name='register'),
+    url('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
