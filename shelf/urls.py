fa92bdf5836606d8ceed8884a125006a328b0474
amirreza <amirrezazand@hotmail.com>
2020-04-07 17:29:50 +0430

    Fix bug in self/url

--- shelf/urls.py
+++ shelf/urls.py
@@ -23,6 +23,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/User', include('Users.urls')),
+    path('api/User/', include('Users.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
