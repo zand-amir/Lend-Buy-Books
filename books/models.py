690454c43f8d4d95ed38b8759e9659f5aca083cf
amirreza <amirrezazand@hotmail.com>
2020-04-08 18:37:31 +0430

    Create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from Users.models import user
+
+
 # Create your models here.
+
+class Books(models.Model):
+    CATEGORY = (
+        ("بدون دسته بندی","بدون دسته بندی"),
+        ("ادبیات", "ادبیات"),
+        ("سفرنامه", "سفرنامه"),
+        ("رمان", "رمان"),
+        ("جغرافیا", "جغرافیا"),
+        ("کتاب مرجع", "کتاب مرجع"),
+        ("ورزشی", "ورزشی"),
+        ("حقوقی", "حقوقی"),
+        ("آشپزی", "آشپزی"),
+        ("روانشناسی", "روانشناسی"),
+        ("کتاب مصور", "کتاب مصور"),
+        ("تاریخی", "تاریخی"),
+        ("اجتماعی", "اجتماعی"),
+
+    )
+    Owner = models.ManyToManyField(user)
+    Title = models.CharField(max_length=100, blank=False, default='بدون عنوان')
+    Description = models.TextField(default='بدون توضیحات', blank=False)
+    categories = models.CharField(max_length=100, blank=False, null=False, choices=CATEGORY, default = "بدون دسته بندی")
+
+
+
+    def __unicode__(self):
+        return self.title
+
+
+
+    def __str__(self):
+        return self.title
+
