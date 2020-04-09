65d50ad1ce4af4da8f17242fc3ec9bdebb28fe22
amirreza <amirrezazand@hotmail.com>
2020-04-09 17:48:37 +0430

    Add media directory

--- shelf/urls.py
+++ shelf/urls.py
@@ -14,15 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static , serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/User/', include('Users.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# if settings.DEBUG :
+#     urlpatterns += [
+#         url(r'^media/(?P<path>.*)$' , serve , {
+#             'document_root' : settings.MEDIA_ROOT
+#         } )
+#     ]
+
+# https://www.youtube.com/watch?v=1Icsv0yyFyM
+
