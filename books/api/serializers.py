43a40cfd6f9ea1d93b26cea15c14f78152c7cb31
amirreza <amirrezazand@hotmail.com>
2020-04-09 17:53:48 +0430

    Create (CreateBookSerializer)

---
+++ books/api/serializers.py
@@ -0,0 +1,29 @@
+
+from rest_framework import serializers
+from books.models import Books
+
+class CreateBookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Books
+        fields = [
+            'id',
+            'Title',
+            'Description',
+            'BookIMG',
+            'BookIMG2',
+            'Categories',
+            'Publish_date',
+            'Author',
+            'ISBN',
+            'Publisher',
+            'PDF_Book',
+            'Audio_Book',
+
+
+        ]
+        extra_kwargs = {'BookIMG2': {'required': False} ,
+                        'PDF_Book':{'required': False},
+                        'Audio_Book' : {'required': False}
+                        }
+
