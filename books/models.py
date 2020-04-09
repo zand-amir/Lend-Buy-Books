91ed242f75238a03d0f0314ab81f75953bd4cbc9
amirreza <amirrezazand@hotmail.com>
2020-04-09 17:50:50 +0430

    Create book and owners model

--- books/models.py
+++ books/models.py
@@ -24,10 +24,19 @@ class Books(models.Model):
 
 
     )
-    Owner = models.ForeignKey(user , related_name='books',on_delete=models.CASCADE)
+    # Owner = models.ForeignKey(user , related_name='books',on_delete=models.CASCADE)
     Title = models.CharField(max_length=100, blank=False, default='بدون عنوان')
     Description = models.TextField(default='بدون توضیحات', blank=False)
     Categories = models.CharField(max_length=100, blank=False, null=False, choices=CATEGORY, default = "بدون دسته بندی")
+    Publish_date = models.CharField(max_length=100 , blank=False , default='تاریخ ذکر نشده است')
+    Author = models.CharField(max_length=100 , blank= False , default='بدون نویسنده')
+    ISBN = models.CharField(max_length=100, blank=False, default='بدون شابک')
+    Publisher = models.CharField(max_length=100 , blank= False , default= 'بدون ناشر')
+    BookIMG = models.ImageField(upload_to='Image/%Y/%m/%d/', blank=True)
+    BookIMG2 = models.ImageField(upload_to='Image/%Y/%m/%d/', blank=True)
+    PDF_Book = models.FileField(upload_to='PDF/%Y/%m/%d/', blank=True  , name= 'کتاب الکترونیکی')
+    Audio_Book = models.FileField(upload_to='Audios/%Y/%m/%d/', blank=True , name='کتاب صوتی')
+
 
 
 
@@ -35,7 +44,13 @@ class Books(models.Model):
         return self.Title
 
 
-
     def __str__(self):
         return self.Title
+
+class Proposal_Book(models.Model):
+
+
+    Owner = models.ManyToManyField(user , related_name='have_book')
+    Proposed_book = models.ManyToManyField(Books , related_name='proposedBook')
+
 
