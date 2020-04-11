db8401faf9060a79c5c79b60494b855d5fcf53bf
amirreza <amirrezazand@hotmail.com>
2020-04-11 21:08:00 +0430

    Add GET OBJ ID seializer

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,29 +1,49 @@
 
 from rest_framework import serializers
-from books.models import Books
+
+from books.models import(
+
+    Books ,
+    Proposed_Book
+
+)
 
 class CreateBookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Books
-        fields = [
-            'id',
-            'Title',
-            'Description',
-            'BookIMG',
-            'BookIMG2',
-            'Categories',
-            'Publish_date',
-            'Author',
-            'ISBN',
-            'Publisher',
-            'PDF_Book',
-            'Audio_Book',
-
-
-        ]
+        fields = "__all__"
+        #     [
+        #     'id',
+        #     'Title',
+        #     'Description',
+        #     'BookIMG',
+        #     'BookIMG2',
+        #     'Categories',
+        #     'Publish_date',
+        #     'Author',
+        #     'ISBN',
+        #     'Publisher',
+        #     'PDF_Book',
+        #     'Audio_Book',
+        #
+        #
+        # ]
         extra_kwargs = {'BookIMG2': {'required': False},
                         'PDF_Book':{'required': False},
                         'Audio_Book' : {'required': False}
                         }
+
+
+
+class Proposed_BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Proposed_Book
+        fields = '__all__'
+
+
+
+class FindOBJID(serializers.Serializer):
+    Object_ID=serializers.IntegerField()
 
