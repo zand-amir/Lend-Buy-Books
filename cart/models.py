0ab174d36b05da0668d7f64bc4f66ce6f475fe61
amirreza <amirrezazand@hotmail.com>
2020-04-12 18:56:58 +0430

    Create model for shopping

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
+from Users.models import user
+from books.models import Proposed_Book
+
 # Create your models here.
+
+
+class Cart(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True , null=True, blank=True )
+    status = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(user, related_name="carts" , on_delete=models.CASCADE)
+    items = models.ManyToManyField(Proposed_Book)
+
+class Order(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True , null=True, blank=True)
+    cart = models.ForeignKey(Cart, blank=True, null=True , on_delete=models.SET_NULL)
+    items = models.ManyToManyField(Proposed_Book)
+    user = models.ForeignKey(user, related_name="orders" ,  on_delete=models.CASCADE)
+
+
+
+
+#auto_now_add=True, null=True, blank=True
+# class Product(models.Model):
+#     product = models.OneToOneField(Proposed_Book, related_name="Item_order", on_delete=models.SET_NULL, null=True)
+#     #date_created = models.DateTimeField(auto_now_add=True)
+#     status = models.CharField(max_length=255)
+#     #product_name = models.CharField(max_length=255)
+#     original_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     our_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     origianl_url = models.TextField()
+#     image_url = models.TextField()
+#     html = models.TextField()
+#     options = models.TextField()
+#     imageData = models.TextField()
+
+# class OrderItem(models.Model):
+#     product = models.OneToOneField(Proposed_Book ,related_name="Item_order", on_delete=models.SET_NULL, null=True)
+#     is_ordered = models.BooleanField(default=False)
+#     date_added = models.DateTimeField(auto_now=True)
+#     date_ordered = models.DateTimeField(null=True)
+#
+#     # def __str__(self):
+#     #     return self.product.name
+#
+#
+# class Order(models.Model):
+#     ref_code = models.CharField(max_length=15)
+#     owner_want_to_buy = models.ForeignKey(user, on_delete=models.SET_NULL, null=True)
+#     is_ordered = models.BooleanField(default=False)
+#     items = models.ManyToManyField(OrderItem)
+#     date_ordered = models.DateTimeField(auto_now=True)
+#
+#     def get_cart_items(self):
+#         return self.items.all()
+#
+#     def get_cart_total(self):
+#         return sum([item.product.Offered_price for item in self.items.all()])
+#
+#     def __str__(self):
+#         return '{0} - {1}'.format(self.owner, self.ref_code)
+#
+#
+# class Transaction(models.Model):
+#     profile = models.ForeignKey(user, on_delete=models.CASCADE)
+#     #token = models.CharField(max_length=120)
+#     order_id = models.CharField(max_length=120)
+#     amount = models.DecimalField(max_digits=100, decimal_places=2)
+#     success = models.BooleanField(default=True)
+#     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+#
+#     def __str__(self):
+#         return self.order_id
+#
+#     class Meta:
+#         ordering = ['-timestamp']
+#
+
+
