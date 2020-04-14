4ff0c3295772690df3c5feb64723f3c0d84ebb39
amirreza <amirrezazand@hotmail.com>
2020-04-14 19:50:26 +0430

    Set view for Cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+
+from .models import (
+                Cart
+)
+
+
+from cart.api.serializers import (
+                CartSerializer
+)
+
+
+
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
