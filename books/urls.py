334b1eab4286ee3f6d5e9b6666485c1305475ee8
amirreza <amirrezazand@hotmail.com>
2020-04-14 18:53:10 +0430

    Add Url propose book

--- books/urls.py
+++ books/urls.py
@@ -6,10 +6,18 @@ from django.conf.urls import url
 
 from django.contrib import admin
 
-from books.views import CreateBookAPIView
+from books.views import( CreateBookAPIView,
+                Proposed_bookCreationAPI
+                         )
+
+
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     url(r'CreateBook/', CreateBookAPIView.as_view(), name='create'),
+    url(r'^Book-propose/$', Proposed_bookCreationAPI.as_view() , name = 'propose_book'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
