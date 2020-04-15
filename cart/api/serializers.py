2315a359407cb8685cbbdb062a5f3c2de67c8a70
amirreza <amirrezazand@hotmail.com>
2020-04-15 17:23:04 +0430

    Edit cart seializer

--- cart/api/serializers.py
+++ cart/api/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from cart.models  import Cart
 
 
+
 class CartSerializer(serializers.HyperlinkedModelSerializer):
     created_by = serializers.CurrentUserDefault()
 
@@ -14,3 +15,20 @@ class CartSerializer(serializers.HyperlinkedModelSerializer):
             'created_by',
             'order_items',
         ]
+
+
+# class ProductsSerializer(serializers.ModelSerializer):
+#
+#
+#     class Meta:
+#         model = Products
+#         fields = '__all__'
+#
+#
+#
+# class OrdersSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Orders
+#         fields = '__all__'
