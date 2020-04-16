e0e575e7ffa7cf3e13bb15680bda27926f3c3b4d
ArashDz <adashti7799@gmail.com>
2020-04-16 15:52:19 +0430

    Change the comment app so its on the book titles instead of the offers

--- comment/models.py
+++ comment/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from books.models import Proposed_Book
+from books.models import Books
 from Users.models import user
 
 
 
 
 class Comment(models.Model):
-    Addressed_Book = models.OneToOneField(Proposed_Book,on_delete=models.CASCADE)
+    Addressed_Book = models.ForeignKey(Books, null=True, on_delete=models.CASCADE)
     Comment_Author = models.OneToOneField(user,on_delete=models.CASCADE)
     Comment_Text = models.TextField(default='نظر کاربر')
 
-    Owners_HR = models.CharField(max_length=8,choices=[('0','bad'),('1','medium'),('2','good'),('n/a','N/A')],default=('n/a'))        #Owner`s Honesty Rating
+    #Owners_HR = models.CharField(max_length=8,choices=[('0','bad'),('1','medium'),('2','good'),('n/a','N/A')],default=('n/a'))        #Owner`s Honesty Rating
 
     def __str__(self):
         return '"' + str(self.Comment_Author)+ '"' + '`s Comment On ' + '<<' + str(self.Addressed_Book) + ">>"
