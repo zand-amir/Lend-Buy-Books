410eeb809bc33ff1e5d3c921baeea8829008cdd3
amirreza <amirrezazand@hotmail.com>
2020-04-18 18:50:27 +0430

    Fix migrational bugs

--- books/urls.py
+++ books/urls.py
@@ -8,8 +8,9 @@ from django.contrib import admin
 
 from books.views import( CreateBookAPIView,
                 Proposed_bookCreationAPI,
-                         Borrow_bookCreationAPI,
-                         StartBorrowAPI
+                Borrow_bookCreationAPI,
+                StartBorrowAPI ,
+                Book_all_View
                          )
 
 
@@ -23,5 +24,6 @@ urlpatterns = [
     url(r'^Book-propose/$', Proposed_bookCreationAPI.as_view() , name = 'propose_book'),
     url(r'^Book-BorrowOffer/$', Borrow_bookCreationAPI.as_view() , name = 'Borrow_offer'),
     url(r'^Book-BorrowStart/$', StartBorrowAPI.as_view() , name = 'Borrow_start'),
+    url('Books-View/',Book_all_View.as_view(),name='BooksView'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
