b62d4201c524cb762fe34d72972d034706264236
amirreza <amirrezazand@hotmail.com>
2020-04-21 18:33:05 +0430

    Create a model for book let

--- booklets/models.py
+++ booklets/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from Users.models import user
 
 # Create your models here.
+
+
+class Booklets(models.Model):
+    CATEGORY = (
+
+        ("بدون دسته بندی" , "بدون دسته بندی"),
+        ("پزشکی", "پزشکی"),
+        ("علوم پایه", "علوم پایه"),
+        ("زیست شناسی", "زیست شناسی"),
+        ("زمین شناسی", "زمین شناسی"),
+        ("مهندسی", "منهدسی"),
+        ("روانشناسی", "روانشناسی"),
+        ("حقوق", "حقوق"),
+
+    )
+
+    Owner = models.ForeignKey(user , related_name="Book_let_owner" , on_delete=models.CASCADE )
+    Title = models.CharField(max_length=100, blank=False, default='بدون ذکر نام')
+    Category = models.CharField(max_length=100, blank=False, null=False, choices=CATEGORY, default=CATEGORY[0][1])
+    Description = models.TextField(default='بدون توضیحات', blank=False)
+    Course_name = models.CharField(max_length=100, blank=False, default='بدون ذکر نام درس')
+    Price = models.CharField(max_length=100 , blank= False , default='بدون قیمت')
+    University_name = models.CharField(max_length=100, blank=False, default='بدون ذکر نام دانشگاه')
+    Professor_name = models.CharField(max_length=100, blank=False, default='بدون ذکر نام استاد')
+    Semester = models.CharField(max_length=100, blank=False, default='بدون ذکر ترم')
+    BookletIMG = models.ImageField(upload_to='BookLET/%Y/%m/%d/', blank=True , name='تصویر جزوه')
+    PDF_FILE = models.FileField(upload_to='BookLETPDF/%Y/%m/%d/', blank=True, name='پی دی اف')
+    # Arash should make a decision
+    # PDF_Validate_File = models.FileField(upload_to='ValPDF/%Y/%m/%d/', blank=True, name=' پی دی اف')
+
+    def __unicode__(self):
+        return self.Title
+
+    def __str__(self):
+        return self.Title
+
+
+
+
