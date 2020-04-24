b427efe99e67edccac51151ea8f0446a738c32ad
amirreza <amirrezazand@hotmail.com>
2020-04-24 17:36:18 +0430

    Add list of all booklets url

--- booklets/urls.py
+++ booklets/urls.py
@@ -7,9 +7,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import BookletCreationAPI
+from .views import (BookletCreationAPI ,
+                    BookletsView
+                    )
 urlpatterns = [
 
     url(r'CreateBooklet/', BookletCreationAPI.as_view() , name='Create_booklets'),
+    url(r'Book-let-list/' , BookletsView.as_view() , name='List_booklets')
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
