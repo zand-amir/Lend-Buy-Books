367febc99989f89308b88f32757d23cb48ca1d7f
amirreza <amirrezazand@hotmail.com>
2020-04-26 16:50:00 +0430

    Add backend search in settings.py

--- shelf/settings.py
+++ shelf/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
