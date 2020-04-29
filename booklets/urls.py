041d756a79a9a2da37a8da160f414a6fdbf02751
amirreza <amirrezazand@hotmail.com>
2020-04-29 18:48:39 +0430

    Add booklets advance search

--- booklets/urls.py
+++ booklets/urls.py
@@ -10,7 +10,8 @@ from django.conf.urls.static import static
 from .views import (BookletCreationAPI ,
                     BookletsView ,
                     ViewBookLetsAPI ,
-                    Searching_Booklets_View
+                    Searching_Booklets_View ,
+                    Booklets_Advance_Search
                     )
 urlpatterns = [
 
@@ -18,6 +19,7 @@ urlpatterns = [
     url(r'Book-let-list/' , BookletsView.as_view() , name='List_booklets') ,
     url(r'BookLetsView/', ViewBookLetsAPI.as_view(), name='View'),
     url(r'SearchBookLetsView/',Searching_Booklets_View.as_view(),name='SearchBooklets'),
+    url(r'BookLetsAdvancedSearch/',Booklets_Advance_Search.as_view({'get': 'list'}),name='booklet_advance_search'),
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
