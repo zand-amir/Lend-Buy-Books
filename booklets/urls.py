bf33080190987becea4634a98f9d377ec61ffab8
amirreza <amirrezazand@hotmail.com>
2020-04-29 18:45:07 +0430

    Add booklets search Url

--- booklets/urls.py
+++ booklets/urls.py
@@ -9,12 +9,15 @@ from django.conf.urls.static import static
 
 from .views import (BookletCreationAPI ,
                     BookletsView ,
-                    ViewBookLetsAPI
+                    ViewBookLetsAPI ,
+                    Searching_Booklets_View
                     )
 urlpatterns = [
 
     url(r'CreateBooklet/', BookletCreationAPI.as_view() , name='Create_booklets'),
     url(r'Book-let-list/' , BookletsView.as_view() , name='List_booklets') ,
     url(r'BookLetsView/', ViewBookLetsAPI.as_view(), name='View'),
+    url(r'SearchBookLetsView/',Searching_Booklets_View.as_view(),name='SearchBooklets'),
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
