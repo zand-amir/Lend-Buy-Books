d83f78d4987c8a23c717e44321094ebc6dda2d04
amirreza <amirrezazand@hotmail.com>
2020-05-27 16:40:25 +0430

    Create a test for sigm-up :)

--- Users/tests.py
+++ Users/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from random_username.generate import generate_username
+
+from .models import user
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .api.serializers import UserInformationSerializer
+
+
+class RegistrationTestCase(APITestCase):
+    def test_sign_up(self):
+        random_username = generate_username()[0]
+        data = {
+            "username": random_username ,
+            "password":"SomeStrongPassword",
+            "email": random_username + '@me.com',
+            "first_name":"HisName",
+            "last_name":"HisLastName",
+            "phone_number":"09126687452",
+            "address":"This is the test so computer doesnt have any address or location",
+            "postal_code":"1545685215"
+
+        }
+        response = self.client.post("/api/User/sign-up/",data)
+
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
