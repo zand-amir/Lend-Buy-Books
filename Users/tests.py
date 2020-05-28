8aaf84239e6df758a756f4318002e181f623076d
amirreza <amirrezazand@hotmail.com>
2020-05-28 16:23:37 +0430

    Add login Test case

--- Users/tests.py
+++ Users/tests.py
@@ -27,3 +27,26 @@ class RegistrationTestCase(APITestCase):
         response = self.client.post("/api/User/sign-up/",data)
 
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+
+class LoginTestCase(APITestCase):
+    def test_sign_up(self):
+        random_username = generate_username()[0]
+        data = {
+            "username": random_username,
+            "password": "SomeStrongPassword",
+            "email": random_username + '@me.com',
+            "first_name": "HisName",
+            "last_name": "HisLastName",
+            "phone_number": "09126687452",
+            "address": "This is the test so computer doesnt have any address or location",
+            "postal_code": "1545685215"
+
+        }
+        registeration = self.client.post("/api/User/sign-up/",data)
+        data_Login = {
+            "username":random_username,
+            "password": "SomeStrongPassword"
+        }
+        response = self.client.post("/api/User/token/",data_Login)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
