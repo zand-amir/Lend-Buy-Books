d4ebcf882dd8e428854ae28319af16d9a1f1bb53
amirreza <amirrezazand@hotmail.com>
2020-05-29 23:28:52 +0430

    Add Authenticated user TestCase :)

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+import json
+import tempfile
 
-# Create your tests here.
+from random_username.generate import generate_username
+
+import requests
+
+from PIL import Image
+
+from .models import user
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase ,APIClient
+from rest_framework import status
+
+class CreationBookTestCase(APITestCase):
+    def setUp(self) :
+        self.random_username = generate_username()[0]
+        data = {
+            "username": self.random_username,
+            "password": "SomeStrongPassword",
+            "email": self.random_username + '@me.com',
+            "first_name": "HisName",
+            "last_name": "HisLastName",
+            "phone_number": "09126687452",
+            "address": "This is the test so computer doesnt have any address or location",
+            "postal_code": "1545685215"
+
+        }
+        self.registeration = self.client.post("/api/User/sign-up/", data)
+        self.client = APIClient()
+        log_data = {
+            "username" : self.random_username,
+            "password" : "SomeStrongPassword"
+        }
+        self.log = self.client.post("/api/User/token/" , log_data)
+        self.Token = self.log.data["access"]
+
+
+
+
+
+    def test_CreateBook(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+self.Token)
+        image = Image.new('RGB' , (100,100))
+        temp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image.save(temp_file)
+        temp_file.seek(0)
+        data = {
+            "Title": "TestTitle",
+            "Description": "TestDescription",
+            "Categories": "بدون دسته بندی",
+            "Publish_date": "TestDate",
+            "publish_series": "TestSeries",
+            "Author": "TestAuthor",
+            "Price": "TestPrice",
+            "ISBN": "TestISBN",
+            "Publisher": "TestPub",
+            "BookIMG": temp_file
+        }
+        response = self.client.post("/api/Books/CreateBook/",data=data)
+        self.assertEqual(status.HTTP_201_CREATED , response.status_code)
+
+
+
+
+
