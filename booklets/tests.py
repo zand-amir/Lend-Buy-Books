029d2b6e411fc3b02e36dc9522b2f6714f33f85c
amirreza <amirrezazand@hotmail.com>
2020-06-05 16:44:49 +0430

    Implement Simple search booklets

--- booklets/tests.py
+++ booklets/tests.py
@@ -120,4 +120,14 @@ class BookletsTestCase(APITestCase):
         response = self.client.get("/api/Booklets/BookLetsView/?q={}".format(BookletID))
         self.assertEqual(response.status_code , status.HTTP_200_OK)
 
+    def test_BookletsSearch(self):
+
+        requestToGetID = self.client.get("/api/Booklets/BookLetsView/")
+        BookletID = requestToGetID.data[0].get('id')
+        response = self.client.get("/api/Booklets/SearchBookLetsView/?q={}".format(BookletID))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
 
