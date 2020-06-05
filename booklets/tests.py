42259255195bb20634679fd23b595c59f4cc671c
amirreza <amirrezazand@hotmail.com>
2020-06-05 16:20:03 +0430

    Implement test for createBooklet

--- booklets/tests.py
+++ booklets/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+import json
+import tempfile
 
+from random_username.generate import generate_username
+
+import random
+
+import requests
+
+from PIL import Image
+
+
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase ,APIClient
+from rest_framework import status
 # Create your tests here.
+
+class CreationBookletsTestCase(APITestCase):
+    def setUp(self) :
+        self.random_username = generate_username()[0]
+        data = {
+            "username": self.random_username,
+            "password": "SomeStrongPassword",
+            "email": self.random_username + '@me.com',
+            "first_name": "HisName",
+            "last_name": "HisLastName",
+            "phone_number": "09126687452",
+            "address": "This is the test so computer doesnt have any address or location",
+            "postal_code": "1545685215"
+
+        }
+        self.registeration = self.client.post("/api/User/sign-up/", data)
+        self.client = APIClient()
+        log_data = {
+            "username" : self.random_username,
+            "password" : "SomeStrongPassword"
+        }
+        self.log = self.client.post("/api/User/token/" , log_data)
+        self.Token = self.log.data["access"]
+
+
+
+
+
+    def test_CreateBooklets(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+self.Token)
+        data_for_booklets = {
+            "Title" : "Test Booklet Name" ,
+            "Category" : "بدون دسته بندی" ,
+            "Professor_name" : "Test Name",
+            "Description" : "Test Descriptions" ,
+            "Course_name" : "Test course" ,
+            "University_name" : "Test UNI Name" ,
+            "Semester" : "Test Semester"
+        }
+        response = self.client.post("/api/Booklets/CreateBooklet/" , data_for_booklets)
+        self.assertEqual(response.status_code , status.HTTP_201_CREATED)
+
+
+
+
