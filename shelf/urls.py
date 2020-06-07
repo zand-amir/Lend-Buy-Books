b46785986ea05e985ad602251b5fba42dfc252e4
amirrezazand <you@example.com>
2020-06-07 01:02:40 -0700

    Add new URL to join room

--- shelf/urls.py
+++ shelf/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path(r'api/Booklets/', include("booklets.urls")),
     path('comments/',include("comment.urls")),
     path(r'api/Actions/', include("cart.urls")) ,
+    path('chat/',include('chat.urls' , namespace='chat'))
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
