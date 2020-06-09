43c179da36a2106ca7733e75417811631efbd81e
ArashDz <adashti7799@gmail.com>
2020-06-09 19:24:56 +0430

    Create message and conversation models

--- Users/models.py
+++ Users/models.py
@@ -10,3 +10,13 @@ class user(User):
     phone_number = models.CharField(max_length=15)
     img = models.ImageField(default='media/profile/images.png',upload_to='profile')
     credit = models.IntegerField(default=0)
+
+class Message(models.Model):
+    Text = models.CharField(max_length=100,blank = False)
+    Date = models.DateTimeField()
+
+class Conversation(models.Model):
+    Participant1 = models.ForeignKey(user, on_delete = models.CASCADE,related_name='participant1', unique = False)
+    Participant2 = models.ForeignKey(user, on_delete = models.CASCADE,related_name='participant2', unique = False)
+    Log = models.ManyToManyField(Message)
+
