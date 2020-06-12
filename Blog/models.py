d0b7a163a1ae3e936ace83fc51e6cb22874682ab
ArashDz <adashti7799@gmail.com>
2020-06-12 22:54:23 +0430

    Create Post and Comment models

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from Users.models import user
 
-# Create your models here.
+class BComment(models.Model):
+    Author = models.ForeignKey(user,on_delete=models.CASCADE,null=False)
+    Date = models.DateTimeField(null=False)
+    Title = models.CharField(max_length=40,null=False,blank=False,default="بدون عنوان")
+    Text = models.CharField(max_length=100,null=False,blank=False)
+    Likes = models.IntegerField(null=False,default=0)
+
+class BPost(models.Model):
+    Author = models.ForeignKey(user,on_delete=models.CASCADE,null=False)
+    Date = models.DateTimeField(null=False)
+    Thumbnail = models.ImageField(upload_to='Blog/thumbnails/%Y/%m/%d',blank = True)
+    Title = models.CharField(max_length = 40,null=False,blank=False)
+    Text = models.TextField(null=False,blank=False)
+    PostImage = models.ImageField(upload_to='Blog/posts/%Y/%m/%d',blank = True)
+    Likes = models.IntegerField(null=False,default=0)
+    Comments = models.ManyToManyField(BComment)
