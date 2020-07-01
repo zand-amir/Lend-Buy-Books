8ee1121a4a23c9054d9fbefe7f2f2c779226793c
amirreza <amirrezazand@hotmail.com>
2020-07-01 15:22:18 +0430

    Remove comments

--- Blog/urls.py
+++ Blog/urls.py
@@ -25,14 +25,4 @@ urlpatterns = [
     url(r'Comment/Like/(?P<BCommentID>.+)/$', BCommentLike.as_view() , name = 'com_like'),
     url(r'Comment/Dislike/(?P<BCommentID>.+)/$', BCommentDislike.as_view() , name = 'com_dislike'),
 
-
-    # url(r'CreatePost/', BPostCreate.as_view(), name='create'),
-    # url(r'SubmitComment/(?P<BPostID>.+)/$', BCommentSubmit.as_view() , name = 'com_sub'),
-    # url(r'ViewPosts/', BPostView.as_view() , name = 'post_view'),
-    # url(r'RetrievePost/(?P<BPostID>.+)/$', BPostRetrieve.as_view() , name = 'post_retrieve'),
-    # url(r'LikePost/(?P<BPostID>.+)/$', BPostLike.as_view() , name = 'post_like'),
-    # url(r'DislikePost/(?P<BPostID>.+)/$', BPostDislike.as_view() , name = 'post_dislike'),
-    # url(r'LikeComment/(?P<BCommentID>.+)/$', BCommentLike.as_view() , name = 'com_like'),
-    # url(r'DislikeComment/(?P<BCommentID>.+)/$', BCommentDislike.as_view() , name = 'com_dislike'),
-   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
