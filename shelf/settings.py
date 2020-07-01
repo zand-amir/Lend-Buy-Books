07e7a12ddf155082c590cbaa36eb71c2afed3e0b
amirreza <amirrezazand@hotmail.com>
2020-07-01 14:03:48 +0430

    Add swagger scheme

--- shelf/settings.py
+++ shelf/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
